26346171c17f30004ba54b3fea198a0af1083a0f
aleksipirttimaa <4463880+aleksipirttimaa@users.noreply.github.com>
2021-02-26 22:24:16 +0200

    performance fixes, update docs (Read more)
    fixes:
    opus_sm didn't flush stdout after each result, causing mentionable performance regression, as sm_exporter simply had to wait for the line-editing buffer to fill up
    
    this fix was introduced in aleksipirttimaa/opus_sm@c32fab41f0336a505888673838ccadeedca68407
    
    new features:
    - analysis result metric
    - url -label
    - improved herding
    - example grafana-dashboard

--- README.md
+++ README.md
@@ -20,7 +20,21 @@ You'll need a compiled `opus_sm_demo` executable and ffmpeg installed. This scri
 ```
 # HELP sm_music_probability Probability of music in stream
 # TYPE sm_music_probability gauge
-sm_music_probability 0.999795
+sm_music_probability{url="..."} 0.999795
+```
+
+### Result instrumentation
+
+This is a histogram measuring time between analysis results, you can use it to select an appropriate `scrape_interval`.
+
+```
+# HELP sm_analysis_result_s Instrumenting analysis executable output
+# TYPE sm_analysis_result_s histogram
+sm_analysis_s_bucket{le="0.005",url="..."} 26.0
+  ...
+sm_analysis_result_s_bucket{le="+Inf",url="..."} 791.0
+sm_analysis_result_s_count{url="..."} 791.0
+sm_analysis_result_s_sum{url="..."} 156.07464370007
 ```
 
 ### Stream retries
@@ -30,7 +44,7 @@ This counter is incremented when `ffmpeg` or `opus_sm_demo` exit, resulting in t
 ```
 # HELP sm_stream_retries_total Incremented on retry
 # TYPE sm_stream_retries_total counter
-sm_stream_retries_total 0.0
+sm_stream_retries_total{url="..."} 0.0
 ```
 
 ## opus_sm
@@ -39,12 +53,28 @@ You can find a fork of opus_sm that adds support for streaming here:
 
 https://github.com/aleksipirttimaa/opus_sm
 
+The build instructions in the aforementioned repository work just fine. On Debian you'll need to install some packages:
+
+```sh
+sudp apt install build-essential automake libtool
+```
+
+Just to recap building:
+
+```sh
+./autogen.sh
+./configure
+make
+```
+
+the resulting binary is `./opus_sm_demo`
+
 ## Running
 
 1. Make sure you have `opus_sm_demo` and `ffmpeg`
 
 ```sh
-which `opus_sm_demo` && which `ffmpeg`
+which opus_sm_demo && which ffmpeg
 ```
 
 2. Install pipenv, which is used for dependency management
@@ -64,3 +94,17 @@ python -m pipenv install
 ```sh
 python -m pipenv run python src/main.py https://stream.example.com/example.m3u8
 ```
+
+## Add to prometheus
+
+Add to your `prometheus.yml`:
+
+```
+scrape_configs:
+- job_name: 'speechmusic'
+    scrape_interval: 1s
+    static_configs:
+    - targets: ['localhost:9069']
+```
+
+A good `scrape_interval` depends on the incoming stream transport and encoding, as analysis can only be performed after an entire frame of the encoded audio has been received.
--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 '''
+2021 (c) Aleksi Pirttimaa under the MIT license
+
 Speech music discrimination from a stream using opus_sm exported as a prometheud metric
 
 The input audio is received by ffmpeg
@@ -15,7 +17,7 @@ import argparse
 import subprocess
 import time
 
-from prometheus_client import start_http_server, Counter, Gauge
+from prometheus_client import start_http_server, Counter, Gauge, Histogram
 
 
 
@@ -23,6 +25,12 @@ FFMPEG = "ffmpeg"
 OPUS_SM = "opus_sm_demo"
 
 DEFAULT_PORT = "9069"
+# Also: command line flag
+
+HERD_SUBPROCESSES = True
+# while True: loop in __main__ ensures that if the subprocesses exit
+# or produce unparseable output, they are restarted. This behaviour
+# does not except all python exceptions.
 
 
 
@@ -32,14 +40,25 @@ def assert_available(command):
 
 
 
+class StreamAnalyzerError(Exception):
+    pass
+
+class StreamAnalyzerMetrics():
+    def __init__(self):
+        self.probability = Gauge("sm_music_probability", "Probability of music in stream", ['url'])
+        self.result_summary = Histogram("sm_analysis_result", "Instrumenting analysis executable output", ['url'], unit='s')
+        self.retries = Counter("sm_stream_retries", "Incremented on retry", ['url'])
+
 class StreamAnalyzer():
-    def __init__(self, url, gauge):
+    def __init__(self, url, metrics):
         self._url = str(url)
-        self._gauge = gauge
 
         # subprocess
         self._ffmpeg = None
         self._analyzer = None
+
+        self._metrics = metrics
+
 
     def run(self):
         self._start = time.time()
@@ -47,6 +66,7 @@ class StreamAnalyzer():
         ffmpeg_args = [
             "-i",
             self._url,
+            "-vn",
             "-f",
             "s16le",
             "-ar",
@@ -62,29 +82,33 @@ class StreamAnalyzer():
         self._analyzer = subprocess.Popen((OPUS_SM, "-"), stdin=self._ffmpeg.stdout, stdout=subprocess.PIPE)
 
         while True:
-            if self._ffmpeg.poll() != None:
-                # ffmpeg has terminated
-                break
+            with self._metrics.result_summary.labels(self._url).time():
+                if self._ffmpeg.poll() != None:
+                    # ffmpeg has terminated
+                    break
 
-            # read lines from analyzer
-            line = self._analyzer.stdout.readline().decode('utf-8')
-            if not line:
-                # opus_sm has terminated
-                break
+                # read lines from analyzer
+                line = self._analyzer.stdout.readline().decode('utf-8')
+                if not line:
+                    # opus_sm has terminated
+                    break
 
-            # parse line
-            split = line.split(" ", 1)
-            seconds = float(split[0])
-            probability = float(split[1].rstrip("\n"))
+                # parse line
+                split = line.split(" ", 1)
+                try:
+                    _seconds = float(split[0])
+                    probability = float(split[1].rstrip("\n"))
+                except IndexError as err:
+                    raise StreamAnalyzerError("Parsing analyzer output failed") from err
 
-            # set gauge
-            self._gauge.set(probability)
+                # set gauge
+                self._metrics.probability.labels(self._url).set(probability)
 
     def __del__(self):
         if self._ffmpeg:
-            self._ffmpeg.kill()
+            self._ffmpeg.terminate()
         if self._analyzer:
-            self._ffmpeg.kill()
+            self._analyzer.terminate()
 
 
 
@@ -107,12 +131,14 @@ if __name__ == '__main__':
     # prometheus client
     start_http_server(port)
 
-    gauge = Gauge("sm_music_probability", "Probability of music in stream")
-    retries = Counter("sm_stream_retries", "Incremented on retry")
+    metrics = StreamAnalyzerMetrics()
 
-    while True:
-        analyzer = StreamAnalyzer(url, gauge)
-        analyzer.run()
+    while HERD_SUBPROCESSES:
+        analyzer = StreamAnalyzer(url, metrics)
+        try:
+            analyzer.run()
+        except StreamAnalyzerError as err:
+            print(err)
         # rate limit retry
         time.sleep(5)
-        retries.inc()
+        metrics.retries.labels(url).inc()
